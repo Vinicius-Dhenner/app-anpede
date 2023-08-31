00f84289e6c0a5b329f3941ada61d880473d0054
VINICIUS DHENNER MARINS DE OLIVEIRA <08217630160@ifms.edu.br>
2023-08-30 21:35:51 -0400

    fim de aula 30/08/2023

--- src/app/paginas/login/login.page.ts
+++ src/app/paginas/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(public nav : NavController) {
+    
+   }
+
+  logar() {
+    this.nav.navigateForward('home');
+  }
+
+  registrar() {
+    this.nav.navigateForward('add-associado');
+  }
 
   ngOnInit() {
   }
